b9158424c0b4678a0b8a25d82e2448164c8d7290
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2022-09-06 00:12:34 +0530

    File upload after second bootcamp

---
+++ myscripts.js
@@ -0,0 +1,7 @@
+
+document.getElementById("change").onclick=function()
+		{
+document.getElementById("change").innerHTML="CV HERE";
+  document.getElementById("change").style.color="white";
+  alert('hello buddy');
+		}
